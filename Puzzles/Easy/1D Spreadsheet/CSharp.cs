36dd13316b7229b92a94253bb79854a94f5dcbc7
Nicklas <nicklasbeyerlydersen@gmail.com>
2020-09-30 23:49:59 +0200

    1D Spreadsheet | Almost Done

---
+++ Puzzles/Easy/1D Spreadsheet/CSharp.cs
@@ -0,0 +1,348 @@
+/********************************************/
+/*                                          */
+/* CodinGame.com Solutions by Nicklas185105 */
+/*                                          */
+/* Puzzle: Horse-racing Duals               */
+/* Difficulty: Easy                         */
+/* Date solved: 30.09.2020                  */
+/*                                          */
+/********************************************/
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] operation = new string[N];
+        string[] arg1 = new string[N];
+        string[] arg2 = new string[N];
+        for (int i = 0; i < N; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            operation[i] = inputs[0];
+            arg1[i] = inputs[1];
+            arg2[i] = inputs[2];
+        }
+        int[] output = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+
+            // Write an answer using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            if(operation[i].Equals("VALUE"))
+                output[i] = VALUE(arg1, arg2, output, i, operation, false);
+            else
+            {
+                if (operation[i].Equals("ADD"))
+                {
+                    output[i] = ADD(arg1, arg2, output, i, operation, false);
+                }
+                else if (operation[i].Equals("SUB"))
+                {
+                    output[i] = SUB(arg1, arg2, output, i, operation, false);
+                }
+                else // MULT
+                {
+                    output[i] = MULT(arg1, arg2, output, i, operation, false);
+                }
+            }
+        }
+
+        foreach(int n in output)
+            Console.WriteLine(n);
+    }
+
+    static int VALUE(string[] arg1, string[] arg2, int[] output, int i, string[] operation, bool newI)
+    {
+        if (arg1[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+
+            bool ta = false;
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+
+            return (ta ? a : output[a]);
+        }
+        else
+            return int.Parse(arg1[i]);
+    }
+
+    static int ADD(string[] arg1, string[] arg2, int[] output, int i, string[] operation, bool newI)
+    {
+        if (arg1[i].Split('$').First().Equals(string.Empty) && arg2[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+            int.TryParse(arg2[i].Split('$').Last(), out int b);
+            
+            bool ta = false;
+            bool tb = false;
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+
+            if (b > i || newI)
+            {
+                tb = true;
+                if (operation[b].Equals("VALUE"))
+                    b = VALUE(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("ADD"))
+                    b = ADD(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("SUB"))
+                    b = SUB(arg1, arg2, output, b, operation, true);
+                else
+                    b = MULT(arg1, arg2, output, b, operation, true);
+            }
+            
+            return (ta ? a : output[a]) + (tb ? b : output[b]);
+        }
+        else if (arg1[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+
+            bool ta = false;
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+
+            return (ta ? a : output[a]) + int.Parse(arg2[i]);
+        }
+        else if (arg2[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg2[i].Split('$').Last(), out int b);
+
+            bool tb = false;
+
+            if (b > i || newI)
+            {
+                tb = true;
+                if (operation[b].Equals("VALUE"))
+                    b = VALUE(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("ADD"))
+                    b = ADD(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("SUB"))
+                    b = SUB(arg1, arg2, output, b, operation, true);
+                else
+                    b = MULT(arg1, arg2, output, b, operation, true);
+            }
+            return int.Parse(arg1[i]) + (tb ? b : output[b]);
+        }
+        else
+        {
+            int.TryParse(arg1[i], out int a);
+            int.TryParse(arg2[i], out int b);
+            return a + b;
+        }
+    }
+
+    static int SUB(string[] arg1, string[] arg2, int[] output, int i, string[] operation, bool newI)
+    {
+        if (arg1[i].Split('$').First().Equals(string.Empty) && arg2[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+            int.TryParse(arg2[i].Split('$').Last(), out int b);
+
+            bool ta = false;
+            bool tb = false;
+            
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+
+            if (b > i || newI)
+            {
+                tb = true;
+                if (operation[b].Equals("VALUE"))
+                    b = VALUE(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("ADD"))
+                    b = ADD(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("SUB"))
+                    b = SUB(arg1, arg2, output, b, operation, true);
+                else
+                    b = MULT(arg1, arg2, output, b, operation, true);
+            }
+            
+            return (ta ? a : output[a]) - (tb ? b : output[b]);
+        }
+        else if (arg1[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+
+            bool ta = false;
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+
+            return (ta ? a : output[a]) - int.Parse(arg2[i]);
+        }
+        else if (arg2[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg2[i].Split('$').Last(), out int b);
+
+            bool tb = false;
+
+            if (b > i || newI)
+            {
+                tb = true;
+                if (operation[b].Equals("VALUE"))
+                    b = VALUE(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("ADD"))
+                    b = ADD(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("SUB"))
+                    b = SUB(arg1, arg2, output, b, operation, true);
+                else
+                    b = MULT(arg1, arg2, output, b, operation, true);
+            }
+            return int.Parse(arg1[i]) - (tb ? b : output[b]);
+        }
+        else
+        {
+            int.TryParse(arg1[i], out int a);
+            int.TryParse(arg2[i], out int b);
+            return a - b;
+        }
+    }
+
+    static int MULT(string[] arg1, string[] arg2, int[] output, int i, string[] operation, bool newI)
+    {
+        if (arg1[i].Split('$').First().Equals(string.Empty) && arg2[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+            int.TryParse(arg2[i].Split('$').Last(), out int b);
+
+            bool ta = false;
+            bool tb = false;
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+            
+            if (b > i || newI)
+            {
+                tb = true;
+                if (operation[b].Equals("VALUE"))
+                    b = VALUE(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("ADD"))
+                    b = ADD(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("SUB"))
+                    b = SUB(arg1, arg2, output, b, operation, true);
+                else
+                    b = MULT(arg1, arg2, output, b, operation, true);
+            }
+            
+            return (ta ? a : output[a]) * (tb ? b : output[b]);
+        }
+        else if (arg1[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg1[i].Split('$').Last(), out int a);
+
+            bool ta = false;
+            if (a > i || newI)
+            {
+                ta = true;
+                if (operation[a].Equals("VALUE"))
+                    a = VALUE(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("ADD"))
+                    a = ADD(arg1, arg2, output, a, operation, true);
+                else if (operation[a].Equals("SUB"))
+                    a = SUB(arg1, arg2, output, a, operation, true);
+                else
+                    a = MULT(arg1, arg2, output, a, operation, true);
+            }
+
+            return (ta ? a : output[a]) * int.Parse(arg2[i]);
+        }
+        else if (arg2[i].Split('$').First().Equals(string.Empty))
+        {
+            int.TryParse(arg2[i].Split('$').Last(), out int b);
+
+            bool tb = false;
+
+            if (b > i || newI)
+            {
+                tb = true;
+                if (operation[b].Equals("VALUE"))
+                    b = VALUE(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("ADD"))
+                    b = ADD(arg1, arg2, output, b, operation, true);
+                else if (operation[b].Equals("SUB"))
+                    b = SUB(arg1, arg2, output, b, operation, true);
+                else
+                    b = MULT(arg1, arg2, output, b, operation, true);
+            }
+            return int.Parse(arg1[i]) * (tb ? b : output[b]);
+        }
+        else
+        {
+            int.TryParse(arg1[i], out int a);
+            int.TryParse(arg2[i], out int b);
+            return a * b;
+        }
+    }
+}
